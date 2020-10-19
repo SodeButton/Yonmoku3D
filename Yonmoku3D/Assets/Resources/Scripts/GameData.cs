de01a24197ab7f2a386c655697b98f972d5855fe
Button <sodebotannisei.2002@gmail.com>
2020-10-19 20:13:07 +0900

    update

--- Yonmoku3D/Assets/Resources/Scripts/GameData.cs
+++ Yonmoku3D/Assets/Resources/Scripts/GameData.cs
@@ -8,6 +8,13 @@ public class GameData : MonoBehaviour {
 	static public float epsilon = 0.2f;
 
 	static public List<int[,]> QValue = new List<int[,]>();
+
+	public struct NetWork {
+		bool isPutting;
+	}
+
+	static public NetWork[,,] network;
+
 	static public List<List<Stone>> NetWorks = new List<List<Stone>>();
 	static public List<List<Stone>> LastNetWorks = new List<List<Stone>>();
 }
